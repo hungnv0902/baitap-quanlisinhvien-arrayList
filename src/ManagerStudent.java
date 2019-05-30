df6003a1f43dec995d34cf99fe2fd299942555eb
hungnv0902 <hungnv0902@gmail.com>
2019-05-30 22:02:20 +0700

    add

--- src/ManagerStudent.java
+++ src/ManagerStudent.java
@@ -1,27 +1,26 @@
 import interFace.MethodManagerStudent;
 import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Comparator;
+import java.util.Collections;
 
-public class ManagerStudent extends Student implements MethodManagerStudent<ManagerStudent> {
-    ArrayList<ManagerStudent> students =  new ArrayList<ManagerStudent>();
+public class ManagerStudent implements MethodManagerStudent<Student> {
+    ArrayList<Student> students =  new ArrayList<Student>();
     public int count = 0;
 
     public ManagerStudent() {
     }
 
-    public ManagerStudent(ArrayList<ManagerStudent> students) {
+    public ManagerStudent(ArrayList<Student> students) {
         this.students = students;
     }
 
-    public ManagerStudent(int id, String name, int age, String address, float gpa, ArrayList<ManagerStudent> students) {
-        super(id, name, age, address, gpa);
-        this.students = students;
-    }
 
-    public ArrayList<ManagerStudent> getStudents() {
+    public ArrayList<Student> getStudents() {
         return students;
     }
 
-    public void setStudents(ArrayList<ManagerStudent> students) {
+    public void setStudents(ArrayList<Student> students) {
         this.students = students;
     }
 
@@ -34,7 +33,7 @@ public class ManagerStudent extends Student implements MethodManagerStudent<Mana
     }
 
     @Override
-    public int addStudent(ManagerStudent student) {
+    public int addStudent(Student student) {
         if(students.contains(student)) {
             return -1;
         } else {
@@ -46,19 +45,51 @@ public class ManagerStudent extends Student implements MethodManagerStudent<Mana
     }
 
     @Override
-    public boolean editStudent(ManagerStudent student) {
+    public boolean editStudent(Student student) {
         if(students.contains(student)) {
-            
+            for (Student st : students) {
+                if (st.getId() == student.getId()) {
+                    st.setAddress(student.getAddress());
+                    st.setAge(student.getAge());
+                    st.setGpa(student.getGpa());
+                    st.setName(student.getName());
+                    return true;
+                }
+            }
         }
+            return false;
     }
 
     @Override
     public boolean removeStudent(int id) {
+        for (Student st: students) {
+            if(st.getId() == id) {
+                students.remove(st);
+                count --;
+                return true;
+            }
+        }
+
         return false;
     }
 
     @Override
     public void sortStudent() {
+        Collections.sort(students, new Comparator<Student>() {
+            @Override
+            public int compare(Student o1, Student o2) {
+                if(o1.getGpa() < o2.getGpa()) {
+                    return 1;
+                } else if (o1.getGpa() == o2.getGpa()) {
+                    return 0;
+                } else if (o1.getGpa() > o2.getGpa()) {
+                    return -1;
+                }
+            }
+            }
+
+        });
+
 
     }
-}
+
